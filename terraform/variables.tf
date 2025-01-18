477af7c0eddf7d36dea07ed97f254bc96f2ae4d5
idror67 <idror1967@gmail.com>
2025-01-19 00:59:00 +0200

    third commit

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+default = "us-central"
+}
+
+variable "root_pass"{
+default = "this-is-not-a-safe-password"
+}
+
+variable "ssh_keys" {
+    default = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDSLFiVX+b25tXnsYMUnDU42e9qA5cdruoRjkNWImxR3 default",
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVfxb5JqHR5b0uzEm9vBqff8MX4kTWrpDPMlfuqZr3p default"]
+    }
+
+variable "label" {
+    default = "HW_Instance"
+    }
+
+variable "linode_token" {
+    default = "43bf5d1300c95905a7deddc2c12f7fd2e99dcac3c2ef8189b18d3b89de55a3e7"
+    }  
+
+variable "github_token" {
+    default = "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"
+    }  
